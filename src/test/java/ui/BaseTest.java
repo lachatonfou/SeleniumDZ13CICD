581490f94b266b06c8640c1c8ea6e74033bec1d6
lachatonfou <maniakitten@mail.ru>
2025-05-20 21:39:18 +1000

    добавила remote

--- src/test/java/ui/BaseTest.java
+++ src/test/java/ui/BaseTest.java
@@ -4,6 +4,12 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Map;
 
 public class BaseTest {
     protected WebDriver driver;
@@ -11,7 +17,8 @@ public class BaseTest {
 
     @BeforeEach
     void setup() {
-        driver = new ChromeDriver();
+        //driver = new ChromeDriver();
+        initDriver();
         driver.get(BASE_URL);
         driver.manage().window().maximize();
     }
@@ -19,5 +26,24 @@ public class BaseTest {
     @AfterEach
     void tearDown() {
         driver.quit();
+    }
+
+    private void initDriver() {
+        String remoteUrl = System.getenv("SELENIUM_REMOTE_URL");
+        if (remoteUrl != null || !remoteUrl.isEmpty()) {
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--headless");  // Add headless mode
+            options.addArguments("--disable-gpu"); // Switch off GPU, because we don't need it in headless mode
+            options.addArguments("--no-sandbox"); // Switch off sandbox to prevent access rights issues
+            options.addArguments("--disable-dev-shm-usage"); // Use /tmp instead of /dev/shm
+            options.setCapability("goog:loggingPrefs", Map.of("browser", "ALL"));
+            try {
+                driver = new RemoteWebDriver(new URL(remoteUrl), options);
+            } catch (MalformedURLException e) {
+                throw new RuntimeException("Malformed URL for Selenium Remote WebDriver", e);
+            }
+        } else {
+            driver = new ChromeDriver();
+        }
     }
 }
--- src/test/java/ui/BaseTestWithPOM.java
+++ src/test/java/ui/BaseTestWithPOM.java
@@ -1,10 +1,17 @@
 package ui;
 
+import io.qameta.allure.Allure;
 import io.qameta.allure.Feature;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Map;
 
 @Feature("POM")
 public class BaseTestWithPOM {
@@ -12,12 +19,33 @@ public class BaseTestWithPOM {
 
     @BeforeEach
     void setup() {
-        driver = new ChromeDriver();
+        //driver = new ChromeDriver();
+        initDriver();
         driver.manage().window().maximize();
     }
 
     @AfterEach
     void tearDown() {
         driver.quit();
+    }
+
+    private void initDriver() {
+        String remoteUrl = System.getenv("SELENIUM_REMOTE_URL");
+        Allure.addAttachment("remote", remoteUrl);
+        if (remoteUrl != null || !remoteUrl.isEmpty()) {
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--headless");  // Add headless mode
+            options.addArguments("--disable-gpu"); // Switch off GPU, because we don't need it in headless mode
+            options.addArguments("--no-sandbox"); // Switch off sandbox to prevent access rights issues
+            options.addArguments("--disable-dev-shm-usage"); // Use /tmp instead of /dev/shm
+            options.setCapability("goog:loggingPrefs", Map.of("browser", "ALL"));
+            try {
+                driver = new RemoteWebDriver(new URL(remoteUrl), options);
+            } catch (MalformedURLException e) {
+                throw new RuntimeException("Malformed URL for Selenium Remote WebDriver", e);
+            }
+        } else {
+            driver = new ChromeDriver();
+        }
     }
 }
